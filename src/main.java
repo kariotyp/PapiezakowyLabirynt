52f6859b52fa705decece748a34400d96d72f1d0
osv-java <74246680+osv-java@users.noreply.github.com>
2020-11-18 18:07:06 +0100

    KURWA

--- src/main.java
+++ src/main.java
@@ -1,7 +1,6 @@
-
 public class main {
-    public static void main(String[] args) throws InterruptedException{
-        rooms rooms = new rooms();
-        rooms.room1();
+        public static void main (String[]args) throws InterruptedException {
+            rooms rooms = new rooms();
+            rooms.room1();
+        }
     }
-}
