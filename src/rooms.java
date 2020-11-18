edaf1bfb3e51f566ad86c427051ab529acad86e6
arauki <bartekkijak286@gmail.com>
2020-11-18 21:12:19 +0100

    WSZYSTKO CHUJU POZA 3
    KONRAD KURWO NIE WIEM CO Z TYM TWOIM POKOJEM Z GUZIORAMI ZAJEBANYM

--- src/rooms.java
+++ src/rooms.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 
 public class rooms extends functions {
     Scanner scanner;
@@ -52,7 +53,12 @@ public class rooms extends functions {
         System.out.println("[1] - Krzysztof Krawczyk\n[2] - Zbigniew Wodecki\n[3] - Kamil Bednarek");
         System.out.println("Nad drzwiami zaś widzisz kolejny wiersz \"Me życie to bajka,w której gram role grajka,\nnie obchodzi mnie rasa, religia czy pejs,\nja po prostu chce popłynąć statkiem w piękny rejs, kim jestem?\"");
         System.out.println("Które drzwi wybierasz?");
-        drzwi = scanner.nextInt();
+        try {
+            drzwi = scanner.nextInt();
+        }catch (Exception exception) {
+            scanner.nextInt();
+            wrongDoorsValue();
+        }
         switch (drzwi) {
             case 1 -> przejsciePrzezDrzwi = true;
             case 2, 3 -> przejsciePrzezDrzwi = false;
@@ -64,27 +70,28 @@ public class rooms extends functions {
     }
 
     public void room3() {
-        String[] answers = {"puszka", "butelka", "telewizor"};
-        System.out.println("Pamiętasz.... kolejny szczegół..... byłeś...... nie, jesteś uczniem.... 7 klasa..... prawdopodobnie...... ale co tu robisz?.... jak sie tu znalazłeś?........ kolejne drzwi.... gdzie ty jesteś....... co?.....");
-        System.out.println("Widzisz 3 guziki: " + answers[0] + ", " + answers[1] + ", " + answers[2] + ".\nNad przyciskami wisi zagadka.");
-        String odp;
-        System.out.println("\"Na fotelu siedze ja,\nczasem tu są niezłe jaja,\nsiedze tutaj cały dzień,\nco oglądam proszę ja cie?\"");
-        odp = scanner.next();
-        if (odp.equals(answers[2])) { //zamieniłem zwykłe odpowiedzi na tablice
-            System.out.println("\"Siedze dalej, pudło gra, może wypić coś się da?\nSam ja jestem proszę ciebie, cóż ja wypić mogę w tym niebie?\"");
+            String[] answers = {"puszka", "butelka", "telewizor"};
+            System.out.println("Pamiętasz.... kolejny szczegół..... byłeś...... nie, jesteś uczniem.... 7 klasa..... prawdopodobnie...... ale co tu robisz?.... jak sie tu znalazłeś?........ kolejne drzwi.... gdzie ty jesteś....... co?.....");
+            System.out.println("Widzisz 3 guziki: " + answers[0] + ", " + answers[1] + ", " + answers[2] + ".\nNad przyciskami wisi zagadka.");
+            String odp;
+            System.out.println("\"Na fotelu siedze ja,\nczasem tu są niezłe jaja,\nsiedze tutaj cały dzień,\nco oglądam proszę ja cie?\"");
             odp = scanner.next();
-            if (odp.equals(answers[0])) {
-                System.out.println("\"Przyszedł boczek, przyszła i menda,\njak powita gości zrzęda?\"");
+            if (odp.equals(answers[2])) { //zamieniłem zwykłe odpowiedzi na tablice==ZAJEBIŚCIE CHUJU GŁUPI NIE WIEM JAK TO ZROBIĆ
+                System.out.println("\"Siedze dalej, pudło gra, może wypić coś się da?\nSam ja jestem proszę ciebie, cóż ja wypić mogę w tym niebie?\"");
                 odp = scanner.next();
-                if (odp.equals(answers[1])) {
-                    System.out.println("Jam jest Ferdynand Kiepski,\nbestia zamknęła mnie tu i usilnie próbuje wyważyć drzwi,\nostrzegam tego co to czyta aby uciekał czem prędzej przez trzecie drzwi!");
-                    przejsciePrzezDrzwi = true;
-                } else {
-                    System.out.println("Nie potrafisz rozwiązać zagadki, słysząc bestie coraz bliżej w panice przechodzisz przez którekolwiek drzwi z nadzieją że to te dobre ");
-                    przejsciePrzezDrzwi = false;
+                if (odp.equals(answers[0])) {
+                    System.out.println("\"Przyszedł boczek, przyszła i menda,\njak powita gości zrzęda?\"");
+                    odp = scanner.next();
+                    if (odp.equals(answers[1])) {
+                        System.out.println("Jam jest Ferdynand Kiepski,\nbestia zamknęła mnie tu i usilnie próbuje wyważyć drzwi,\nostrzegam tego co to czyta aby uciekał czem prędzej przez trzecie drzwi!");
+                        przejsciePrzezDrzwi = true;
+                    } else {
+                        System.out.println("Nie potrafisz rozwiązać zagadki, słysząc bestie coraz bliżej w panice przechodzisz przez którekolwiek drzwi z nadzieją że to te dobre ");
+                        przejsciePrzezDrzwi = false;
+                    }
                 }
             }
-        }
+
         przejscie(przejsciePrzezDrzwi);
         room4();
     }
@@ -93,7 +100,12 @@ public class rooms extends functions {
         System.out.println("Biegniesz dalej, kolejna zagadka, kolejne rozwidlenie, zupełnie jak w tym hotelu.......... w hotelu?\nDobiegasz do kolejnej zagadki.");
         System.out.println("qij T një R nënës Z nënës E qij qij një esbek ndyrë pështy C një lavire në fytyrë,I në mbrëmje, në mëngjes derisa qij është i vdekur E harruar");
         System.out.println("Masz do wyboru 3 pary drzwi. Które wybierasz?\n[1] - pierwsze drzwi\n[2] - drugie drzwi\n[3] - trzecie drzwi");
-        drzwi = scanner.nextInt();
+        try {
+            drzwi = scanner.nextInt();
+        }catch (Exception exception) {
+            scanner.nextInt();
+            wrongDoorsValue();
+        }
         switch (drzwi) {
             case 1, 2 -> przejsciePrzezDrzwi = false;
             case 3 -> przejsciePrzezDrzwi = true;
@@ -114,7 +126,12 @@ public class rooms extends functions {
         }
         System.out.println("Nad drzwiami widzisz ogniste litery które tworzą napis \"Jak mi dadzą to jem\"");
         System.out.println("Przez które drzwi przechodzisz? [1-5]");
-        drzwi = scanner.nextInt();
+        try {
+            drzwi = scanner.nextInt();
+        }catch (Exception exception) {
+            scanner.nextInt();
+            wrongDoorsValue();
+        }
         drzwi--;
         switch (drzwi) {
             case 0, 2, 3, 4 -> przejsciePrzezDrzwi = false;
@@ -130,7 +147,12 @@ public class rooms extends functions {
         System.out.println("Czujesz w głębi serca, że coraz bardziej bestia zbliża sie do ciebie. Boisz sie jak nigdy dotąd, po ostatniej zagadce czujesz że jeśli nie wytężysz swej głowy i nie pośpieszysz sie z odpowiedziami, ZGINIESZ.");
         System.out.println("Wbiegasz do następnego pokoju, widzisz przed sobą kolejne 2 pary drzwi. Gdy sie rozglądasz, zdajesz sobie sprawe że znajdujesz sie w pokoju w którym znajdują sie różne meble oraz.... łóżko. Czujesz, że bestia jest coraz bliżej i musisz szybko podjąć decyzje - przeszukać łóżko w poszukiwaniu szukając czegoś użytecznego czy uciec przez któreś drzwi");
         System.out.println("[1] - Chowam się pod łóżkiem w poszukianiu czegoś użytecznego\n[2] - Uciekam dalej");
-        drzwi = scanner.nextInt();
+        try {
+            drzwi = scanner.nextInt();
+        }catch (Exception exception) {
+            scanner.nextInt();
+            wrongDoorsValue();
+        }
         animation();
         switch (drzwi) {
             case 1 -> {
@@ -149,7 +171,12 @@ public class rooms extends functions {
         System.out.println("Biegniesz dalej przez kolejne rozwidlenia, przypominasz sobie, że ty uciekałeś już wcześniej, koledzy cie nie zostawili,\nzostali....... zamordowani...... przez tego kultyste w pizzerii....");
         System.out.println("Przed sobą widzisz trzy pary drzwi na których widnieją kolejno napisy napisy:\n na 1 drzwiach \"z warkoczykami\",\nna drugich \"obie\",\nzaś na trzecich \"bez warkoczyków\".\nGdy spoglądasz w górę, widzisz jak w miejscu zagadki tworzą się litery z ognia piekielnego i tworzą napis \"Jakie bestia lubi dziewczynki?\"");
         System.out.println("Przez które drzwi przechodzisz?\n[1] - pierwsze drzwi\n[2] - drugie drzwi\n[3] - trzecie drzwi.");
-        drzwi = scanner.nextInt();
+        try {
+            drzwi = scanner.nextInt();
+        }catch (Exception exception) {
+            scanner.nextInt();
+            wrongDoorsValue();
+        }
         switch (drzwi) {
             case 1, 2 -> przejsciePrzezDrzwi = false;
             case 3 -> przejsciePrzezDrzwi = true;
@@ -163,7 +190,12 @@ public class rooms extends functions {
     public void room8() {
         System.out.println("Biegniesz dalej, kolejne wspomnienia wracają. Pamiętasz tego kultyste, zabrał was na pizze po całodziennym zwiedzaniu, wasza wychowawczyni nie protestowała... czemu?");
         System.out.println("Przed sobą widzisz drzwi z ponumerowane od 1 do 4, na środku korytarza stoi tabliczka na której jest pytanie\n\"W którym filmie shreka główny bohater podpisuje cyrograf? [1-4]\"");
-        drzwi = scanner.nextInt();
+        try {
+            drzwi = scanner.nextInt();
+        }catch (Exception exception) {
+            scanner.nextInt();
+            wrongDoorsValue();
+        }
         switch (drzwi) {
             case 1, 2, 3 -> przejsciePrzezDrzwi = false;
             case 4 -> przejsciePrzezDrzwi = true;
@@ -177,7 +209,12 @@ public class rooms extends functions {
         System.out.println("Biegniesz dalej, pamiętasz że koledzy go polubili i poszli z nim na zaplecze... ty zaś do toalety... dlatego przeżyłeś.");
         System.out.println("Widzisz 3 pary drzwi, każde z nich podpisane innym nazwiskiem:\n[1] - \"Tadeusz Sznuk\"\n[2] - \"Hubert Urbański\"\n[3] - \"Norbi\"\nNad nimi zaś pytanie:\n\"Jak nazywa się prowadzący teleturnieju, w którym sprzedajesz dusze za słodkie kuferki?\"");
         System.out.println("Przez które drzwi przechodzisz?[1-3]");
-        drzwi = scanner.nextInt();
+        try {
+            drzwi = scanner.nextInt();
+        }catch (Exception exception) {
+            scanner.nextInt();
+            wrongDoorsValue();
+        }
         switch (drzwi) {
             case 1 -> przejsciePrzezDrzwi = true;
             case 2, 3 -> przejsciePrzezDrzwi = false;
@@ -191,7 +228,12 @@ public class rooms extends functions {
         System.out.println("Ta miła kelnerka... powiedziała ci gdzie poszli... z ciekawości tam zajrzałeś... nigdy tego widoku nie wymażesz z pamięci. Nie możesz tracić cennego czasu, uciekasz dalej.");
         System.out.println("Widzisz przed sobą 2 pary drzwi z napisami:\n[1] - \"tak\"\n[2] - \"nie\"\nNad drzwiami zaś widnieje pytanie:\n\"Pan Jezus poświęcił się za ludzkość. Czy ty jesteś dość odważny na taką ofiare?\"");
         System.out.println("Które drzwi wybierasz? [1-2]");
-        drzwi = scanner.nextInt();
+        try {
+            drzwi = scanner.nextInt();
+        }catch (Exception exception) {
+            scanner.nextInt();
+            wrongDoorsValue();
+        }
         switch (drzwi) {
             case 1 -> przejsciePrzezDrzwi = false;
             case 2 -> przejsciePrzezDrzwi = true;
@@ -206,7 +248,12 @@ public class rooms extends functions {
         System.out.println("Pamiętasz kolejne rzeczy, uciekłeś z pizzeri... pamiętasz krzyk tej kelnerki... biegłeś ulicą do hotelu w którym była twoja klasa... chyba...");
         System.out.println("Stajesz przed trzema parami drzwi na których są wyryte kolejno odpowiedzi:\n[1] - \"9%\"\n[2] - \"2137%\"\n[3] - \"3%\"\nNad drzwiami widzisz pytanie:\n\"Ile procent ma beczkowane mocne wiśniowe?\"");
         System.out.println("Przez które drzwi decydujesz się przejść? [1-3]");
-        drzwi = scanner.nextInt();
+        try {
+            drzwi = scanner.nextInt();
+        }catch (Exception exception) {
+            scanner.nextInt();
+            wrongDoorsValue();
+        }
         switch (drzwi) {
             case 1 -> przejsciePrzezDrzwi = true;
             case 2, 3 -> przejsciePrzezDrzwi = false;
@@ -220,7 +267,12 @@ public class rooms extends functions {
         System.out.println("Czujesz w głębi serca, że coraz bardziej bestia zbliża sie do ciebie. Boisz sie jak nigdy dotąd, po ostatniej zagadce czujesz że jeśli nie wytężysz swej głowy i nie pośpieszysz sie z odpowiedziami, ZGINIESZ.");
         System.out.println("Wbiegasz do następnego pokoju, widzisz przed sobą kolejne 2 pary drzwi. Gdy sie rozglądasz, zdajesz sobie sprawe że znajdujesz sie w pokoju w którym znajdują sie różne meble oraz.... łóżko. Czujesz, że bestia jest coraz bliżej i musisz szybko podjąć decyzje - przeszukać łóżko w poszukiwaniu szukając czegoś użytecznego czy uciec przez któreś drzwi");
         System.out.println("[1] - Chowam się pod łóżkiem w poszukianiu czegoś użytecznego\n[2] - Uciekam dalej");
-        drzwi = scanner.nextInt();
+        try {
+            drzwi = scanner.nextInt();
+        }catch (Exception exception) {
+            scanner.nextInt();
+            wrongDoorsValue();
+        }
         animation();
         switch (drzwi) {
             case 1 -> {
@@ -239,7 +291,12 @@ public class rooms extends functions {
         System.out.println("Uciekałeś krętymi uliczkami, pieprzone filmy których się naoglądałeś, to tam mówili ci że to zawsze działa... pamiętasz ten smród... pamiętasz... nie... nie pamiętasz... albo nie chcesz pamiętać.");
         System.out.println("Dobiegasz do drzwi które są całe zakrwawione. Widzisz że krew jest wszędzie wymieszana z kremówką, nawet spod drzwi cieknie.");
         System.out.println("[1] - Przechodzę przez drzwi\n[2] - Rozglądam się po pomieszczeniu");
-        drzwi = scanner.nextInt();
+        try {
+            drzwi = scanner.nextInt();
+        }catch (Exception exception) {
+            scanner.nextInt();
+            wrongDoorsValue();
+        }
         animation();
         switch (drzwi) {
             case 1 -> przejsciePrzezDrzwi = false;
@@ -247,7 +304,12 @@ public class rooms extends functions {
                 przejsciePrzezDrzwi = true;
                 System.out.println("Rozglądając się po pomieszczeniu widzisz... okno. Małe okienko, zmieściłbyś sie w nie...");
                 System.out.println("[1] - Wybierasz drzwi\n[2] - Wybierasz okno");
-                drzwi = scanner.nextInt();
+                try {
+                    drzwi = scanner.nextInt();
+                }catch (Exception exception) {
+                    scanner.nextInt();
+                    wrongDoorsValue();
+                }
                 switch (drzwi) {
                     case 1 -> przejsciePrzezDrzwi = false;
                     case 2 -> {
@@ -269,7 +331,12 @@ public class rooms extends functions {
         System.out.println("Ta bestia..... to ona cie porwała..... jedyne co pamiętasz po spotkaniu jej to ból głowy... tylko tyle.... ale to może i dobrze?");
         System.out.println("3... 3 pary d-drzwi... n... nie masz już s-siły... na drzwiach... widnieją... numery...\n[1] - 4\n[2] - 3\n[3] - 5\nNad nimi zaś... pytanie...\n\"Najlepsza część \"Heroes of Might and Magic\"");
         System.out.println("Przez które... drzwi... prze-przechodzisz [1-3]");
-        drzwi = scanner.nextInt();
+        try {
+            drzwi = scanner.nextInt();
+        }catch (Exception exception) {
+            scanner.nextInt();
+            wrongDoorsValue();
+        }
         switch (drzwi) {
             case 1, 3 -> przejsciePrzezDrzwi = false;
             case 2 -> przejsciePrzezDrzwi = true;
@@ -287,7 +354,12 @@ public class rooms extends functions {
         System.out.println("Wystraszony biegniesz dalej, masz dość tego głosu który mówi przez ściany, dobiegasz do kolejnego rozwidlenia/");
         System.out.println("Stoisz przed dwiema para drzwi, na których po kolei widnieją napisy:\n[1] - \"Zbigniew Stonoga\"\n[2] - \"Zbigniew Ziobro\"\nNad drzwiami widzisz pytanie:\n\"Który prześladuje rodzine drugiego? Wybierz mądrze, już prawie jesteś na miejscu\"");
         System.out.println("Przez które drzwi przechodzisz? [1-2]");
-        drzwi = scanner.nextInt();
+        try {
+            drzwi = scanner.nextInt();
+        }catch (Exception exception) {
+            scanner.nextInt();
+            wrongDoorsValue();
+        }
         switch (drzwi) {
             case 1 -> przejsciePrzezDrzwi = false;
             case 2 -> przejsciePrzezDrzwi = true;
@@ -301,7 +373,12 @@ public class rooms extends functions {
         System.out.println("To Watykan, nie ma innej opcji, piwnica watykanu! Brat mnie ostrzegał, abym nie jechał na tą wycieczke... Dobiegasz do wielkiego zdobionego rozwidlenia");
         System.out.println("Stoisz przed trzema pięknie wykończonymi parami drzwi. Czujesz że to już ostatnie przejście, ostatnie drzwi... niestety nie umiesz rozczytać zagadki, jest w niezrozumiałym dla ciebie języku... po włosku? Patrzysz na nią i próbujesz przeczytać:\n\"Pazzo dietro la terza porta, ma rilassati, non sarai ancora in grado di sfuggire alla bestia\"");
         System.out.println("Nie masz zielonego pojęcia jak zagadka ma ci pomóc w wyborze drzwi, jednak musisz podjąć szybko decyzję... przez które drzwi przechodzisz? [1-3]");
-        drzwi = scanner.nextInt();
+        try {
+            drzwi = scanner.nextInt();
+        }catch (Exception exception) {
+            scanner.nextInt();
+            wrongDoorsValue();
+        }
         animation();
         System.out.println("*Powodzenia dzieciaku...*");
         ending();
